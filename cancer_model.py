498e92fe632eee954d76fc188729959628db73d1
Parth SIngh <36064077+ParthSingh0506@users.noreply.github.com>
2022-05-05 14:07:32 +0530

    Update cancer_model.py

--- cancer_model.py
+++ cancer_model.py
@@ -21,21 +21,21 @@ from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state =0)
 classifier.fit(X_train,y_train)
 
-# joblib.dump(classifier,'cancer.pkl')
+joblib.dump(classifier,'cancer.pkl')
 
 #Predicting value on the test set
 
-# mod = joblib.load('cancer.pkl')
+mod = joblib.load('cancer.pkl')
 y_pred = classifier.predict(X_test)
 print(y_pred)
-# #Making of Confusion Matrix 
-# from sklearn.metrics import confusion_matrix
-# cm = confusion_matrix(y_pred,y_test)
-# #print(cm)
-# s = (104+59)/(103+59+4+5)
 
+#Making of Confusion Matrix 
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_pred,y_test)
+print(cm)
+s = (104+59)/(103+59+4+5)
 
-# from sklearn.model_selection import cross_val_score
-# accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
-# print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
-# print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
